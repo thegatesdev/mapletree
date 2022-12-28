8cdc2a1c50e039679c80886f8fab52d1b5da84fb
TheGatesScratch <mtkarels@outlook.com>
2022-12-28 15:23:57 +0100

    Rename some.

--- src/main/java/io/github/thegatesdev/mapletree/data/ExpandableType.java
+++ src/main/java/io/github/thegatesdev/mapletree/data/ExpandableType.java
@@ -10,19 +10,20 @@ import java.util.function.Function;
 
 public class ExpandableType<T> extends DataType<T> implements ReadableDataHolder {
 
-    private final List<Reader<?>> readers = new ArrayList<>();
-    private final Function<DataMap, T> dataType;
+    private final List<Expansion<?>> expansions = new ArrayList<>();
+    private final Function<DataMap, T> baseCreator;
     private final ReadableData readableData;
 
     public ExpandableType(Class<T> type, ReadableData readableData, Function<DataMap, T> baseCreator) {
         super(type);
-        this.dataType = baseCreator;
+        this.baseCreator = baseCreator;
         this.readableData = readableData;
     }
 
-    public <D> void addReader(String key, DataTypeHolder<D> dataType, BiConsumer<D, T> action) {
+    public <D> ExpandableType<T> expand(String key, DataTypeHolder<D> dataType, BiConsumer<D, T> action) {
         readableData.add(key, dataType, null);
-        readers.add(new Reader<>(key, action));
+        expansions.add(new Expansion<>(key, action));
+        return this;
     }
 
     @Override
@@ -33,9 +34,9 @@ public class ExpandableType<T> extends DataType<T> implements ReadableDataHolder
     @Override
     public T read(DataElement dataElement) {
         final DataMap data = readableData.read(dataElement.requireOf(DataMap.class));
-        final T apply = dataType.apply(data);
-        for (Reader<?> reader : readers) {
-            data.ifPrimitive(reader.key, dataPrimitive -> reader.action.accept(dataPrimitive.valueUnsafe(), apply));
+        final T apply = baseCreator.apply(data);
+        for (Expansion<?> expansion : expansions) {
+            data.ifPrimitive(expansion.key, dataPrimitive -> expansion.action.accept(dataPrimitive.valueUnsafe(), apply));
         }
         return apply;
     }
@@ -46,11 +47,11 @@ public class ExpandableType<T> extends DataType<T> implements ReadableDataHolder
         return this;
     }
 
-    private final class Reader<D> {
+    private final class Expansion<D> {
         private final String key;
         private final BiConsumer<D, T> action;
 
-        private Reader(String key, BiConsumer<D, T> action) {
+        private Expansion(String key, BiConsumer<D, T> action) {
             this.key = key;
             this.action = action;
         }

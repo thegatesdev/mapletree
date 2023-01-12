bfb8fb8e449164a537e565a00fe38cd509cedc26
TheGatesScratch <mtkarels@outlook.com>
2023-01-12 14:54:35 +0100

    Remove ReadableFactory.

--- src/main/java/io/github/thegatesdev/mapletree/factory/ReadableFactory.java
+++
@@ -1,7 +0,0 @@
-package io.github.thegatesdev.mapletree.factory;
-
-
-import io.github.thegatesdev.mapletree.data.ReadableDataHolder;
-
-public interface ReadableFactory<G> extends ReadableDataHolder, Factory<G> {
-}

44f12eb0f2989b67aa609be8bed7b335e2dca3b4
TheGatesScratch <mtkarels@outlook.com>
2023-01-17 17:16:08 +0100

    Add DataType list proxy method.

--- src/main/java/io/github/thegatesdev/mapletree/data/DataTypeHolder.java
+++ src/main/java/io/github/thegatesdev/mapletree/data/DataTypeHolder.java
@@ -1,5 +1,11 @@
 package io.github.thegatesdev.mapletree.data;
 
+import java.util.List;
+
 public interface DataTypeHolder<D> {
     DataType<D> getDataType();
+
+    default DataType<List<D>> list() {
+        return getDataType().list();
+    }
 }

b807774883a66b43c83c1a4a9fd8dea5d556536f
TheGatesScratch <mtkarels@outlook.com>
2023-03-04 10:30:44 +0100

    Record primitive and list types info.

--- src/main/java/io/github/thegatesdev/mapletree/data/Readable.java
+++ src/main/java/io/github/thegatesdev/mapletree/data/Readable.java
@@ -5,6 +5,7 @@ import io.github.thegatesdev.maple.data.DataList;
 import io.github.thegatesdev.maple.data.DataMap;
 import io.github.thegatesdev.maple.data.DataPrimitive;
 import io.github.thegatesdev.maple.exception.ElementException;
+import io.github.thegatesdev.mapletree.registry.DataTypeInfo;
 import io.github.thegatesdev.threshold.Threshold;
 
 import java.util.ArrayList;
@@ -21,8 +22,6 @@ public class Readable<D> implements DataType<D> {
     private final Class<D> dataClass;
     private final Function<DataElement, D> readFunction;
     private final String identifier;
-    // CACHE
-    private Readable<List<D>> listType;
 
     public Readable(String identifier, final Function<DataElement, D> readFunction) {
         this(identifier, null, readFunction);
@@ -35,6 +34,8 @@ public class Readable<D> implements DataType<D> {
     }
 
     // --
+
+    // CACHED TYPES
 
     @SuppressWarnings("unchecked")
     private static <D> Readable<D> getOrCreatePrimitive(Class<D> primitiveClass, Supplier<Readable<D>> supplier) {
@@ -52,35 +53,42 @@ public class Readable<D> implements DataType<D> {
     }
 
     private static <D> Readable<List<D>> createList(DataType<D> original) {
-        return new Readable<>(original.id() + "_list", element -> {
+        return DataTypeInfo.get(new Readable<>(original.id() + "_list", element -> {
             final DataList list = element.requireOf(DataList.class);
             final List<D> results = new ArrayList<>(list.size());
             list.forEach(e -> results.add(original.read(e)));
             return results;
-        });
+        })).description("List of '%s'".formatted(original.id())).dataType();
     }
 
 
     public static <D> Readable<D> primitive(Class<D> dataClass) {
-        return getOrCreatePrimitive(dataClass, () -> new Readable<>(dataClass.getSimpleName().toLowerCase(), dataClass, element -> element.requireOf(DataPrimitive.class).requireValue(dataClass)));
+        return primitive(dataClass.getSimpleName().toLowerCase(), dataClass);
     }
 
     public static <D> Readable<D> primitive(String identifier, Class<D> dataClass) {
-        return getOrCreatePrimitive(dataClass, () -> new Readable<>(identifier, dataClass, element -> element.requireOf(DataPrimitive.class).requireValue(dataClass)));
-    }
-
-    public static <D> Readable<D> primitive(String identifier, Class<D> dataClass, Function<DataPrimitive, D> primitiveReader) {
-        return new Readable<>(identifier, dataClass, element -> primitiveReader.apply(element.requireOf(DataPrimitive.class)));
+        return getOrCreatePrimitive(dataClass, () -> DataTypeInfo.get(
+                        new Readable<>(identifier, dataClass, element -> element.requireOf(DataPrimitive.class).requireValue(dataClass)))
+                .description("A " + dataClass.getSimpleName()).dataType()
+        );
     }
 
     public static <E extends Enum<E>> Readable<E> enumeration(Class<E> enumClass) {
-        return getOrCreatePrimitive(enumClass, () -> primitive(enumClass.getSimpleName().toLowerCase(), enumClass, primitive -> {
-            final String s = primitive.stringValue();
-            final E enumValue = Threshold.enumGet(enumClass, s);
-            if (enumValue == null)
-                throw new ElementException(primitive, "'%s' does not contain value %s".formatted(enumClass.getSimpleName(), s));
-            return enumValue;
-        }));
+        return getOrCreatePrimitive(enumClass, () -> DataTypeInfo.get(
+                        single(enumClass.getSimpleName().toLowerCase(), enumClass, primitive -> {
+                            final String s = primitive.stringValue();
+                            final E enumValue = Threshold.enumGet(enumClass, s);
+                            if (enumValue == null)
+                                throw new ElementException(primitive, "'%s' does not contain value %s".formatted(enumClass.getSimpleName(), s));
+                            return enumValue;
+                        }))
+                .description("A " + enumClass.getSimpleName(), "Possible values: " + String.join(", ", Threshold.enumNames(enumClass))).dataType());
+    }
+
+    // OTHER
+
+    public static <D> Readable<D> single(String identifier, Class<D> dataClass, Function<DataPrimitive, D> primitiveReader) {
+        return new Readable<>(identifier, dataClass, element -> primitiveReader.apply(element.requireOf(DataPrimitive.class)));
     }
 
     public static <D> Readable<D> map(String identifier, Class<D> dataClass, Function<DataMap, D> mapReader) {

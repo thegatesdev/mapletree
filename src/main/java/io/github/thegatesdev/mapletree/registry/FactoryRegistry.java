fc6c9c864f1377bc2845e0f7e6bf86f9edbec156
TheGatesScratch <mtkarels@outlook.com>
2023-05-05 15:34:48 +0200

    Remove BasicRegistry and references

--- src/main/java/io/github/thegatesdev/mapletree/registry/FactoryRegistry.java
+++ src/main/java/io/github/thegatesdev/mapletree/registry/FactoryRegistry.java
@@ -6,14 +6,17 @@ import io.github.thegatesdev.maple.exception.ElementException;
 import io.github.thegatesdev.mapletree.data.DataType;
 import io.github.thegatesdev.mapletree.data.Factory;
 import io.github.thegatesdev.mapletree.data.ReadableOptionsHolder;
-import io.github.thegatesdev.mapletree.registry.core.BasicRegistry;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
-public abstract class FactoryRegistry<D, F extends Factory<? extends D> & ReadableOptionsHolder> extends BasicRegistry<String, F> implements Identifiable, DataType<D> {
+public abstract class FactoryRegistry<D, F extends Factory<? extends D> & ReadableOptionsHolder> implements Identifiable, DataType<D> {
     protected final String id;
     private final Function<F, String> keyGetter;
+
+    private final Map<String, F> factories = new HashMap<>();
     private int registered = 0;
 
     protected FactoryRegistry(String id, Function<F, String> keyGetter) {
@@ -51,16 +54,16 @@ public abstract class FactoryRegistry<D, F extends Factory<? extends D> & Readab
     public D read(final DataElement element) {
         final DataMap data = element.requireOf(DataMap.class);
         final String s = data.getString("type");
-        final Factory<? extends D> factory = get(s);
+        final Factory<? extends D> factory = factories.get(s);
         if (factory == null)
             throw new ElementException(data, "specified %s %s does not exist".formatted(id, s));
         return factory.build(data);
     }
 
-    @Override
     public <T extends F> T register(final String key, final T value) {
+        if (factories.putIfAbsent(key, value) != null) throw new RuntimeException("Factory with key '" + key + "' already exists");
         registered++;
-        return super.register(key, value);
+        return value;
     }
 }
 

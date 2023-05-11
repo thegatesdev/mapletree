f83d686850d6c8c2117acb527b0d8cf39b40cf8d
TheGatesScratch <mtkarels@outlook.com>
2023-05-11 15:04:42 +0200

    Factory require ReadableOptionsHolder

--- src/main/java/io/github/thegatesdev/mapletree/registry/FactoryRegistry.java
+++ src/main/java/io/github/thegatesdev/mapletree/registry/FactoryRegistry.java
@@ -2,10 +2,11 @@ package io.github.thegatesdev.mapletree.registry;
 
 import io.github.thegatesdev.mapletree.data.DataType;
 import io.github.thegatesdev.mapletree.data.Factory;
+import io.github.thegatesdev.mapletree.data.ReadableOptionsHolder;
 
 import java.util.Collection;
 
-public abstract class FactoryRegistry<Data, Fac extends Factory<? extends Data>> implements Identifiable, DataType<Data> {
+public abstract class FactoryRegistry<Data, Fac extends Factory<? extends Data> & ReadableOptionsHolder> implements Identifiable, DataType<Data> {
     protected final String id;
 
     protected FactoryRegistry(String id) {
